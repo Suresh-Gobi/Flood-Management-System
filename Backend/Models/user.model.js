515cd25487af071ac9563a72a749acda8c9f3648
Suresh_Gobi <sureshgobi34@gmail.com>
2025-02-13 16:53:44 +0530

    update user model

--- Backend/Models/user.model.js
+++ Backend/Models/user.model.js
@@ -1,42 +1,66 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    otp: {
-        type: String,
-        required: false
-    },
-    resetPasswordOTP: {
-        type: String,
-        required: false
-    },
-    resetPasswordExpires: {
-        type: Date,
-        required: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: false,
+  },
+  first_name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  last_name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  date_of_birth: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  address: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: "admin",
+  },
+  resetPasswordOTP: {
+    type: String,
+    required: false,
+  },
+  resetPasswordExpires: {
+    type: Date,
+    required: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;

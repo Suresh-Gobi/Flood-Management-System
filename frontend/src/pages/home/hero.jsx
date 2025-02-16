ed43b445c5ff06be607467982820f851f602b77d
Suresh_Gobi <sureshgobi34@gmail.com>
2025-02-16 19:52:05 +0530

    refactor hero component: simplify structure and update export method

--- frontend/src/pages/home/hero.jsx
+++ frontend/src/pages/home/hero.jsx
@@ -1,30 +1,7 @@
-import React from 'react';
-import { Container, Typography, Box, Button } from '@mui/material';
+import React from 'react'
 
-export default function Section01() {
-    return (
-        <Container maxWidth={false} style={{ display: 'flex', alignItems: 'center', padding: '2rem', width: '100%' }}>
-            <Box flex={1} style={{ textAlign: 'left' }}>
-                <Box mb={4}>
-                    <Typography variant="h3" component="h1" gutterBottom >
-                        Stay Ahead of Rising Waters with <span style={{ color: 'blue' }}>Smart Flood Monitoring</span>
-                    </Typography>
-                    <Typography variant="h6" component="p" color="textSecondary" >
-                        Real-Time Flood Alerts & Monitoring for Safer Communities
-                    </Typography>
-                </Box>
-                <Box mb={4}>
-                    <Typography variant="body1" component="p" style={{ fontSize: '11px' }}>
-                        Floods can strike without warning, causing severe damage and disruption. Our cutting-edge IoT flood monitoring device provides real-time water level tracking, instant alerts, and actionable insights to help you stay prepared.
-                    </Typography>
-                </Box>
-                <Button variant="contained" color="primary" size="large">
-                    Get Started
-                </Button>
-            </Box>
-            <Box flex={1} style={{ textAlign: 'right' }}>
-                <img src="https://www.icimod.org/wp-content/uploads/2021/04/final-telemetric.jpg" alt="Flood Monitoring" style={{ maxWidth: '100%', height: 'auto' }} />
-            </Box>
-        </Container>
-    );
+export default function hero() {
+  return (
+    <div>hero</div>
+  )
 }

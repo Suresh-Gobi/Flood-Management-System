ecdd40d96daece9b1f58cb5c10662ce6f082ca0b
Suresh_Gobi <sureshgobi34@gmail.com>
2025-02-16 19:30:16 +0530

    enhance signup form: add date picker for date of birth and improve layout

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Card, Form, Input, Typography, notification, Steps } from "antd";
+import { Button, Card, Form, Input, Typography, notification, Steps, DatePicker } from "antd";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
@@ -42,65 +42,66 @@ const SignUp = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Card title={<h1 className="text-2xl">Signup</h1>} className="w-full max-w-xl p-2 rounded-lg shadow-md">
-        <Steps current={currentStep}>
-          <Step title="Account Details" />
-          <Step title="Personal Details" />
-        </Steps>
-        
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <Card title={<h1 className="text-xl font-semibold text-center">Signup</h1>} className="w-full max-w-3xl p-4 rounded-lg shadow-lg">
         {currentStep === 0 && (
-          <Form onFinish={nextStep} autoComplete="off">
-            <Form.Item label="Username" name="username" rules={[{ required: true, message: "Please input your username!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please input your email!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please input your password!" }, { min: 8, message: "Password must be at least 8 characters long!" }]}>
-              <Input.Password />
-            </Form.Item>
-            <Button type="primary" htmlType="submit">Next</Button>
+          <Form onFinish={nextStep} autoComplete="off" layout="vertical">
+            <div className="grid grid-cols-1 ">
+              <Form.Item label="Username" name="username" rules={[{ required: true, message: "Please input your username!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Email" name="email" rules={[{ required: true, message: "Please input your email!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Password" name="password" rules={[{ required: true, message: "Please input your password!" }, { min: 8, message: "Password must be at least 8 characters long!" }]}>
+                <Input.Password />
+              </Form.Item>
+            </div>
+            <Button type="default" htmlType="submit" className="w-full">Next</Button>
           </Form>
         )}
 
         {currentStep === 1 && (
-          <Form onFinish={onFinish} autoComplete="off">
-            <Form.Item label="First Name" name="first_name" rules={[{ required: true, message: "Please enter your first name!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Last Name" name="last_name" rules={[{ required: true, message: "Please enter your last name!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Phone Number" name="phone_number" rules={[{ required: true, message: "Please enter your phone number!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Date of Birth" name="date_of_birth" rules={[{ required: true, message: "Please enter your date of birth!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Address" name="address" rules={[{ required: true, message: "Please enter your address!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Street 1" name="street1">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Street 2" name="street2">
-              <Input />
-            </Form.Item>
-            <Form.Item label="City" name="city" rules={[{ required: true, message: "Please enter your city!" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item label="Province" name="province">
-              <Input />
-            </Form.Item>
-            <Form.Item label="District" name="district">
-              <Input />
-            </Form.Item>
-            <Form.Item label="Country" name="country" rules={[{ required: true, message: "Please enter your country!" }]}>
-              <Input />
-            </Form.Item>
-            <Button onClick={prevStep} className="mr-2">Back</Button>
-            <Button type="primary" htmlType="submit" loading={loading}>{loading ? "Loading..." : "Signup"}</Button>
+          <Form onFinish={onFinish} autoComplete="off" layout="vertical">
+            <div className="grid grid-cols-4 gap-3">
+              <Form.Item label="First Name" name="first_name" rules={[{ required: true, message: "Please enter your first name!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Last Name" name="last_name" rules={[{ required: true, message: "Please enter your last name!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Phone Number" name="phone_number" rules={[{ required: true, message: "Please enter your phone number!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Date of Birth" name="date_of_birth" rules={[{ required: true, message: "Please enter your date of birth!" }]}>
+                <DatePicker style={{ width: '100%' }} />
+              </Form.Item>
+              <Form.Item label="Address" name="address" rules={[{ required: true, message: "Please enter your address!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Street 1" name="street1">
+                <Input />
+              </Form.Item>
+              <Form.Item label="Street 2" name="street2">
+                <Input />
+              </Form.Item>
+              <Form.Item label="City" name="city" rules={[{ required: true, message: "Please enter your city!" }]}>
+                <Input />
+              </Form.Item>
+              <Form.Item label="Province" name="province">
+                <Input />
+              </Form.Item>
+              <Form.Item label="District" name="district">
+                <Input />
+              </Form.Item>
+              <Form.Item label="Country" name="country" rules={[{ required: true, message: "Please enter your country!" }]}>
+                <Input />
+              </Form.Item>
+            </div>
+            <div className="flex justify-between">
+              <Button onClick={prevStep} className="mr-2">Back</Button>
+              <Button type="default" htmlType="submit" loading={loading}>{loading ? "Loading..." : "Signup"}</Button>
+            </div>
           </Form>
         )}
       </Card>

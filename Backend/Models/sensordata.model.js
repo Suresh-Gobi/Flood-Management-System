9f36466399412685519a767a3a10ed064fb6a9d2
Suresh_Gobi <sureshgobi34@gmail.com>
2025-02-16 16:39:04 +0530

    fetching sensor data

---
+++ Backend/Models/sensordata.model.js
@@ -0,0 +1,14 @@
+// models/WaterData.js
+const mongoose = require("mongoose");
+
+const WaterDataSchema = new mongoose.Schema({
+  deviceId: { type: mongoose.Schema.Types.ObjectId, ref: "Device", required: true }, // Reference to Device
+  waterLevel: Number,
+  rainingStatus: String,
+  temperature: Number,
+  airPressure: Number,
+  waterfallLevel: Number,
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("WaterData", WaterDataSchema);
